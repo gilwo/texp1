8eeed6621b323d92c819473ab7f3dcc4791c27f5
gilwo <gilwo@null.net>
2019-10-11 00:15:43 +0300

    fix normalization after padding

--- work2.py
+++ work2.py
@@ -206,9 +206,6 @@ def process_data(data, touch_data, export) -> pd.DataFrame:
 
     _d = data_with_touch
 
-    for c in [TARGET, COMP, FILL1, FILL2]:
-        _d[c] = _d[c] * 10
-
     _d = _d.assign(**{
         NTargtAvg: (_d[COMP] + _d[FILL1] +_d[FILL2]) / 3,
         FILLAvg:   (_d[FILL1] + _d[FILL2]) / 2,
@@ -223,6 +220,10 @@ def process_data(data, touch_data, export) -> pd.DataFrame:
     # pad Target column from touch point
     _d.loc[_d[TIME] >= _d['TOUCH_FIXED'], TARGET] = 10
     
+    # normalized to 100
+    for c in [TARGET, COMP, FILL1, FILL2]:
+        _d[c] = _d[c] * 10
+
     if export is not None:
         if export.split(".")[1] == "xlsx":
             export_csv = export.split(".")[0] + ".csv"
